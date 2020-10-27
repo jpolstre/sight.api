510038b1dabe44aa681f4b926ebb4e1f9157bee7
jpolstre <jpolstre@gmail.com>
2020-10-26 20:03:44 -0400

    Initial commit

---
+++ Models/Server.cs
@@ -0,0 +1,9 @@
+namespace sight.api.Models
+{
+    public class Server
+    { 
+      public int Id { get; set; }
+      public string Name { get; set; }
+      public bool IsOnline { get; set; }
+    }
+}
